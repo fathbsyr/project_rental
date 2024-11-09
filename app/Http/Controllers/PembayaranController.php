4fb53e7a5f4ec4540610fbfbbd1b2f395e84dab2
fatihbasyir <fati22186ti@student.nurulfikri.ac.id>
2024-11-09 08:50:50 +0700

    upload API pembayaran

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Models;
+use App\Models\Pembayaran;
 use App\Http\Resources\ResponsResource;
 use DB;
 
@@ -14,7 +14,18 @@ class PembayaranController extends Controller
      */
     public function index()
     {
-        $pembayaran = DB::table('pembayaran')->get();
+        // $pembayaran = DB::table('pembayaran')->get();
+        $pembayaran = Pembayaran::join('reservasi', 'pembayaran.reservasi_id', '=', 'reservasi.id') 
+        -> join('pelanggan', 'reservasi.pelanggan_id', '=', 'pelanggan.id')
+        -> join('promosi', 'pembayaran.promosi_id', '=', 'promosi.id')
+        -> join('denda', 'pembayaran.denda_id', '=', 'denda.id')
+        -> select('pembayaran.id', 'pembayaran.metode',
+            'pembayaran.tanggal_bayar', 'total_bayar',
+            'pembayaran.status', 'pelanggan.nama as pelanggan',
+            'promosi.diskon as diskon',
+            'denda.keterangan as denda'
+        )
+        -> get();
         return new ResponsResource(true, 'Data Pembayaran', $pembayaran);
     }
 
